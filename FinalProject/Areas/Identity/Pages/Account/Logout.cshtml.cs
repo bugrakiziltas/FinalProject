dce235896bb6b9835b33588cdef0b392732c48d0
bugrakiziltas <bugrahankiziltas@hotmail.com>
2025-06-30 16:28:44 +0300

    Logout eklendi

---
+++ FinalProject/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+
+namespace FinalProject.Areas.Identity.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LogoutModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToPage("/Index");
+        }
+    }
+}
